ca36ec37813e6a91b4e2294b0e752a961438b0cb
Johan <1938159279@qq.com>
2024-12-28 09:23:57 +0800

    folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import { Providers } from "../components/providers";
 
 export const metadata: Metadata = {
   title: "Next.js Starter App",
@@ -21,9 +20,7 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
         />
       </head>
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from "@nextui-org/button";
-
-export default function Home() {
-  return <Button>Hello World</Button>;
-}
